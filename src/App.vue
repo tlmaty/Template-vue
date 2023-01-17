e45f7f6fe251cf256e8ba1288818859db9447acf
tlmaty <t.lambolez@maty.fr>
2023-01-17 10:27:00 +0100

    Hello barre

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,98 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/divers">Divers</router-link>
-  </nav>
-  <router-view/>
+    <HelloBarre content="50% de rabais le premier jour des soldes avec le code SOLDES50 !" background="#00395d" contentLink="Cliquez ici !" color="white" />
+    <slot :hour="hour" :min="min" :sec="sec"></slot>
+    <nav>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link> |
+        <router-link to="/divers">Divers</router-link>
+    </nav>
+    <router-view />
 </template>
 
 <script>
-
+    import HelloBarre from "./components/HelloBarre.vue";
+    export default {
+        name: 'AppVue',
+        components: {
+            HelloBarre
+        },
+        props: {
+            endDate: {
+                type: Date,
+                default() {
+                    return new Date;
+                }
+            },
+            negative: {
+                type: Boolean,
+                default: false
+            }
+        },
+        data() {
+            return {
+                now: new Date(),
+                timer: null
+            }
+        },
+        computed() {
+            function hour() {
+                let h = Math.trunc((this.endDate - this.now) / 1000 / 3600);
+                return h > 9 ? h : '0' + h;
+            }
+            function min() {
+                let m = Math.trunc((this.endDate - this.now) / 1000 / 60) % 60;
+                return m > 9 ? m : '0' + m;
+            }
+            function sec() {
+                let s = Math.trunc((this.endDate - this.now) / 1000) % 60
+                return s > 9 ? s : '0' + s;
+            }
+        },
+        watch: {
+            endDate: {
+                immediate: true,
+                handler(newVal) {
+                    if (this.timer) {
+                        clearInterval(this.timer)
+                    }
+                    this.timer = setInterval(() => {
+                        this.now = new Date()
+                        if (this.negative)
+                            return
+                        if (this.now > newVal) {
+                            this.now = newVal
+                            this.$emit('endTime')
+                            clearInterval(this.timer)
+                        }
+                    }, 1000)
+                }
+            }
+        },
+        beforeUnmount() {
+            clearInterval(this.timer)
+        }
+    }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
     }
-  }
-}
+
+    nav {
+        padding: 30px;
+
+        a {
+            font-weight: bold;
+            color: #2c3e50;
+
+            &.router-link-exact-active {
+                color: #42b983;
+            }
+        }
+    }
 </style>
---
+++ src/components/HelloBarre.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="hellobarre" :style="{ background: this.background, color: this.color }">
+        <p>{{ content }} <a href="#">{{ contentLink }}</a>{{ countDown }}</p>
+        <span>X</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HelloBarre',
+        props: [
+            'content',
+            'background',
+            'color',
+            'contentLink',
+        ],
+        mounted() {
+            let helloBarre = document.querySelector('.hellobarre');
+            let closeBtn = document.querySelector('.hellobarre > span');
+            closeBtn.addEventListener('click', () => {
+                helloBarre.classList.add('closed');
+            });
+        },
+    }
+</script>
+
+<style lang="scss">
+    .hellobarre {
+        //background: #00395d; 
+        position: sticky;
+        top: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        transition: all .5s;
+        min-height: calc(2 * 0.875rem);
+        height: 100%;
+
+        &.closed {
+            height: 0;
+            min-height: inherit;
+
+            p, span, a {
+                font-size: 0rem;
+            }
+        }
+
+        p, span, a {
+            color: inherit;
+            margin: 0;
+            font-size: 0.875rem;
+            line-height: 2;
+            transition: all .5s;
+        }
+
+        p {
+            max-width: 85%;
+        }
+
+        span {
+            right: 1%;
+            position: fixed;
+            cursor: pointer;
+
+            &:hover {
+                opacity: .7;
+            }
+        }
+    }
+</style>
